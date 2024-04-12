5d51c2d7005445d7060615f7f8a31aa2f9191ab5
Anastasia M <morozanastasia2005@gmail.com>
2024-04-12 13:35:16 +0300

    DB is connected succesfully

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerSpec = require('./swagger');
+
+const { sequelize } = require('./src/models/');
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use(cors());
+
+// const userRouter = require('./routes/user.routes');
+// const taskRouter = require('./routes/task.routes');
+// const roomRouter = require('./routes/room.routes');
+
+app.get('/', (req, res) => {
+    res.send(":D");
+});
+
+
+const connectDb = async () => {
+    console.log('Checking db connection...');
+
+    try{
+        await sequelize.authenticate();
+        console.log('Database connection established.');
+    }catch(e){
+        console.log('Database connection failed!', e);
+        process.exit(1);
+    }
+}
+
+app.use(express.json()); 
+
+// app.use('/user', userRouter);
+// app.use("/tasks", taskRouter);
+// app.use('/rooms', roomRouter);
+
+(async () => {
+    await connectDb();
+    const port = process.env.PORT || 8080;
+    app.listen(port, () => {
+        console.log('server running on port', port);
+    });
+})();
+
+    // const port = process.env.PORT || 8080;
+    // app.listen(port, () => {
+    //     console.log('server running on port', port);
+    // });
